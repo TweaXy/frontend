913d8d7d2bdf76cc465d39acc5b5b27d4f48ba0f
ebramatef00 <ebraamatef@outlook.com>
2023-12-02 21:52:36 +0200

    [FR]Added Captcha page to sign up feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
         <>
             <Router>
                 <Routes>
-                    {/* <Route index element={<WelcomePage />} /> */}
-                    <Route index element={<FollowingFollowersPage />} />
+                    <Route index element={<WelcomePage />} /> 
+                    {/*<Route index element={<FollowingFollowersPage />} />*/}
                     <Route
                         path="/forget-password"
                         element={<ForgetPasswordStartPage />}
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -3,6 +3,7 @@ import '../../components/LoginWindowHeader/LoginWindowHeader.css';
 import SignUpPage1 from './SignUpPage1';
 import SignUpPage3 from './SignUpPage3';
 import SignUpPage4 from './SignUpPage4';
+import CaptchaPage from './CaptchaPage';
 import SignUpPage5 from './SignUpPage5';
 import { sendEmailVerification } from '../../apis/EmailVerfication';
 import signup from '../../apis/Signup';
@@ -47,7 +48,9 @@ const SignUpPage = ({ onClose }) => {
                     setwindowOpned,
                     windowOpened
                 );
-                navigate(`home`, { state: { userData: userData, firstTime: true } });
+                navigate(`home`, {
+                    state: { userData: userData, firstTime: true },
+                });
                 setwindowOpned(windowOpened + 1);
             } catch {
                 (err) => {
@@ -98,6 +101,9 @@ const SignUpPage = ({ onClose }) => {
                 />
             )}
             {windowOpened === 2 && (
+                <CaptchaPage nextWindowHandler={nextWindowHandler} />
+            )}
+            {windowOpened === 3 && (
                 <SignUpPage4
                     verficationcode={verficationcode}
                     setverficationcode={setverficationcode}
@@ -105,7 +111,7 @@ const SignUpPage = ({ onClose }) => {
                     nextWindowHandler={nextWindowHandler}
                 />
             )}
-            {windowOpened === 3 && (
+            {windowOpened === 4 && (
                 <SignUpPage5
                     canbeuser={canbeuser}
                     password={password}
@@ -113,7 +119,7 @@ const SignUpPage = ({ onClose }) => {
                     nextWindowHandler={nextWindowHandler}
                 />
             )}
-            {windowOpened === 4 && nextWindowHandler()}
+            {windowOpened === 5 && nextWindowHandler()}
         </div>
     );
 };
