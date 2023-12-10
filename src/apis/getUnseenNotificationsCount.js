c6a78b331215f62dd873b3dc39af2aac06aaffdb
itsHamdySalem <hamdysalem112002@gmail.com>
2023-12-10 14:04:21 +0200

    [ ➕ FR ] Adding APIs handlers for getting notifications and notifications count;

---
+++ src/apis/getUnseenNotificationsCount.js
@@ -0,0 +1,26 @@
+const getUnseenNotificationsCount = async (token) => {
+    const url = `https://tweaxybackend.mywire.org/api/v1/notifications/unseenNotification`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        const responseBody = await response.json();
+
+        if (response.ok) {
+            const data = responseBody.data;
+            return data;
+        }
+
+        throw new Error(`Error: ${responseBody.message}`);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export default getUnseenNotificationsCount;
