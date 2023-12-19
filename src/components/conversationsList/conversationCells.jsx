0e62633eee4d1771f104cac38c1140dcf1a3dd1a
ebramatef00 <ebraamatef@outlook.com>
2023-12-19 15:59:28 +0200

    [ FR ] added conversation cell (not completed)

---
+++ src/components/conversationsList/conversationCells.jsx
@@ -0,0 +1,49 @@
+import './conversationsCells.css';
+
+import { CircularProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import ConversationCell from './conversationCell';
+
+const ConversationCells = ({ users }) => {
+    const [isPageLoading, setIsPageLoading] = useState(users === undefined);
+    const token = useSelector((state) => state.user.token);
+    const myID = useSelector((state) => state.user.user).id;
+
+    useEffect(() => {
+        if (users !== undefined && token && myID) {
+            setIsPageLoading(false);
+            console.log(users);
+        }
+    }, [users, token, myID]);
+
+    if (isPageLoading) {
+        return (
+            <div className="circular-progress-spinner">
+                <CircularProgress />
+            </div>
+        );
+    }
+
+    if (users.length === 0) {
+        return <></>;
+    }
+
+    return (
+        <div className="conversations-cells-container">
+            {users.map((user) => (
+                <ConversationCell
+                    id={user.id}
+                    name={user.name}
+                    username={user.username}
+                    avatar={user.avatar}
+                    bio={user.bio === 'null' ? '' : user.bio}
+                    token={token}
+                    myID={myID}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default ConversationCells;
