e1a986fd0f01f57e7e7add9e8dd52961e4bd05a2
ebramatef00 <ebraamatef@outlook.com>
2023-12-19 15:09:57 +0200

    [FR ]added settings conversation

--- src/components/conversationsList/conversation.jsx
+++ src/components/conversationsList/conversation.jsx
@@ -1,38 +1,76 @@
-import React from "react";
-import ConversationWindowHeader from "./conversationHeader";
-import "./conversation.css"
+import React from 'react';
+import ConversationWindowHeader from './conversationHeader';
+import './conversation.css';
 import Button from '@mui/material/Button';
-import SettingsConversationHeader from "./settingsConversationHeader";
+import SettingsConversationHeader from './settingsConversationHeader';
+import { useSelector } from 'react-redux';
+import UserCell from '../UserCell/UserCell';
+import { useState } from 'react';
 const ListConversation = () => {
- 
-  return (
-    <>
-    <div className="conversation-body">
-    
-    <ConversationWindowHeader />
+    const token = useSelector((state) => state.user.token);
+    const myID = useSelector((state) => state.user.user).id;
+    const user = useSelector((state) => state.user.user);
+    const [isSettingOpened, setIsSettingOpened] = useState(1);
 
+    return (
+        <>
+            <div className="conversation-body">
+                <ConversationWindowHeader />
 
-    <div className="empty-messages-style">
-      <span className="header-inbox-span">Welcome to your inbox!</span>
+                <div className="empty-messages-style">
+                    <span className="header-inbox-span">
+                        Welcome to your inbox!
+                    </span>
 
-      <p className="empty-text-conversation">
-        Drop a line, share posts and more with private conversations between you
-        and others on Tweaxy.
-      </p>
+                    <p className="empty-text-conversation">
+                        Drop a line, share posts and more with private
+                        conversations between you and others on Tweaxy.
+                    </p>
 
-      <Button variant="outlined" className="write-message-button">
-                Write a message
-        </Button>
+                    <Button variant="outlined" className="write-message-button">
+                        Write a message
+                    </Button>
+                </div>
+            </div>
+            {isSettingOpened == 0 && (
+                <div className="settings-converation-style">
+                    <SettingsConversationHeader />
+                    <UserCell
+                        key={myID}
+                        id={myID}
+                        name={user.name}
+                        username={user.username}
+                        avatar={user.avatar}
+                        bio={user.bio === 'null' ? '' : user.bio}
+                        followsMe={user.followsMe}
+                        followedByMe={user.followedByMe}
+                        token={token}
+                        myID={myID}
+                    />
+                    <span className="span-notification-text-settings-conversation">
+                        <span className="text-settings-conversation-page">
+                            Notifications
+                        </span>
+                        <span className="snooze-text-settings-conversation">
+                            Snooze notifications from @{user.username}
+                        </span>
+                    </span>
 
-      </div>
-    </div>
-    <div className="settings-converation-style">
-
-    <SettingsConversationHeader />
-    </div>
-
-    </>
-  );
+                    <div>
+                        <div className="text-settings-conversation-page">
+                            block @{user.username}
+                        </div>
+                        <div className="text-settings-conversation-page">
+                            mute @{user.username}
+                        </div>
+                        <div className="text-settings-conversation-page">
+                            Leave Conversation
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default ListConversation;
