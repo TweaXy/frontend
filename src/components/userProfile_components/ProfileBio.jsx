7e6a7129cd6ef0bf11f19455084ac00c3e73ee9d
ebramatef00 <ebraamatef@outlook.com>
2023-12-24 10:47:43 +0200

    [ REF ] refactory conservation settings

--- src/components/userProfile_components/ProfileBio.jsx
+++ src/components/userProfile_components/ProfileBio.jsx
@@ -105,7 +105,7 @@ const ProfileBio = (props) => {
     return (
         <div className="biocontainer">
             <div className="backgroundImage">
-                <img src={props.coverImage} alt="" />
+                <img src={`https://tweaxybackend.mywire.org/api/v1/images/${props.coverImage}`} alt="" />
             </div>
 
             <div className="profileTitle">
