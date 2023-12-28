45235a1ae492d4ff80b8c4cf54769ed2e3cec9a2
MoMagdy_10 <manga9721@gmail.com>
2023-12-28 13:52:21 +0200

    Adding Some Conversations Apis

---
+++ src/apis/getConverstationMeassages.js
@@ -0,0 +1,38 @@
+const MessageURL = `https://tweaxybackend.mywire.org/api/v1/`;
+const getConversationMessages = async (
+    Conversationid,
+    token,
+    _limit,
+    _offset
+) => {
+    const urlWithQueryParam = `${MessageURL}/conversations/${encodeURIComponent(
+        Conversationid
+    )}?limit=${_limit}?offset=${_offset}`;
+    try {
+        const response = await fetch(urlWithQueryParam, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const responseBody = await response.text();
+        if (response.ok) {
+            const responseData = await JSON.parse(responseBody);
+            if (responseData.status === 'success') {
+                const Messages = responseData.data.items;
+                console.log('Ok ' + Messages);
+                return Messages;
+            } else {
+                console.log('Error From Getting this conversation message');
+                throw new Error(`Error: ${responseData.status}`);
+            }
+        } else {
+            throw new Error(`Error: ${response.status}`);
+        }
+    } catch (err) {
+        console.error('Error From Getting this conversation message: ', err);
+        throw err;
+    }
+};
+export default getConversationMessages;
